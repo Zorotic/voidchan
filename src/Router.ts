d6713ae749f719a12fb44404cb4fa3f67e94b0f3
Zorotic <swftapocalypse1@gmail.com>
2021-01-01 13:35:12 -0500

    implement stats endpoints for images and urls.

--- src/Router.ts
+++ src/Router.ts
@@ -4,6 +4,7 @@ import {
 	FastifyReply as Reply
 } from "fastify";
 import { Connection, createConnection, getRepository, Repository } from "typeorm";
+import { getHeapStatistics } from "v8";
 import { FileEntry, ShortenedURL } from "./entities";
 import { FileUploadReply, ShortenedURLReply } from "./structs";
 import { randomString } from "./util/randomString";
@@ -24,9 +25,14 @@ class APIService {
 
 		this.app.post("/api/providers/sharex", this.handleShareXUpload.bind(this));
 		this.app.post("/api/providers/shortener", this.shortenURL.bind(this));
-		
+
+		/// View statistics.
+		this.app.get("/api/providers/shortener/:id", this.viewShortenURL.bind(this));
+		this.app.get("/api/providers/sharex/:id", this.viewShareXUpload.bind(this));
+
 		/// For uploaded content.
-		this.app.get("/i/:imageId", this.handleGetImage.bind(this));
+		this.app.get("/i/:id", this.handleGetImage.bind(this));
+
 		/// For shortened URLs.
 		this.app.get("/:id", this.handleShortenedURL.bind(this));
 
@@ -58,6 +64,53 @@ class APIService {
 			this.app.log.error(e);
 			process.exit(1);
 		};
+	}
+
+	private async viewShortenURL(req: Request, reply: Reply) {
+		if (!req.headers.authorization || req.headers.authorization !== process.env.UPLOAD_SECRET) {
+			reply.status(401);
+			reply.header("Content-Type", "text/plain");
+			return "You are unable to access this endpoint.";
+		}
+		const id = (req.params as any).id;
+		const url = await this.urls.findOne({ id });
+
+		if (!url) {
+			reply.header("Content-Type", "text/plain");
+			reply.status(404);
+			return "The wizards have been unable to find the url you are looking for!";
+		}
+
+		return {
+			id: url.id,
+			stats: {
+				clicks: url.redirects
+			}
+		}
+	}
+
+	private async viewShareXUpload(req: Request, reply: Reply) {
+		if (!req.headers.authorization || req.headers.authorization !== process.env.UPLOAD_SECRET) {
+			reply.status(401);
+			reply.header("Content-Type", "text/plain");
+			return "You are unable to access this endpoint.";
+		}
+		const id = (req.params as any).id;
+
+		const image = await this.files.findOne({ id });
+		if (!image) {
+			reply.header("Content-Type", "text/plain");
+			reply.status(404);
+			return "The wizards have been unable to find the file you are looking for!";
+		}
+
+		return {
+			imageId: image.id,
+			stats: {
+				views: image.views,
+				uploadDate: image.uploadDate
+			}
+		}
 	}
 
 	private async shortenURL(req: Request, reply: Reply) {
@@ -104,11 +157,14 @@ class APIService {
 			return "The wizards have been unable to find what you are looking for!";
 		}
 
+		// Update stats.
+		await this.urls.update({ id }, { redirects: url.redirects + 1});
+
 		reply.redirect(301, url.destUrl);
 	}
 
 	private async handleGetImage(req: Request, reply: Reply) {
-		const id = (req.params as any).imageId;
+		const id = (req.params as any).id;
 
 		const file = await this.files.findOne({ id });
 		if (!file) {
@@ -117,13 +173,17 @@ class APIService {
 			return "Image not found!";
 		}
 
+		// Update stats.
+		await this.files.update({ id }, { views: file.views + 1});
+
+
 		reply.header("Content-Type", file.mimetype);
 
 		return file.buffer;
 	}
 
 
-	// This is untyped due to the mimetype support we need.
+	/// This is untyped due to the mimetype support we need.
 	private async handleShareXUpload(req: any, reply: any) {
 
 		if (!req.headers.authorization || req.headers.authorization !== process.env.UPLOAD_SECRET) {
@@ -135,7 +195,6 @@ class APIService {
 		const data = await req.file();
 
 		const token = randomString(7, false);
-		/// TODO: We should have a better way to parse the type than this.
 		const mimetype = data.mimetype.split("/")[1];
 		const fileBuffer = await data.toBuffer();
 
