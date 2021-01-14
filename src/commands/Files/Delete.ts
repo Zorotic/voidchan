aa3dcb1ee55504aaf5bf0ff60444bb7e8293d571
Zorotic <swftapocalypse1@gmail.com>
2021-01-14 17:25:53 -0500

    change wording and use message.util where possible.

--- src/commands/Files/Delete.ts
+++ src/commands/Files/Delete.ts
@@ -25,13 +25,15 @@ export default class PingCommand extends Command {
 			return response.content.toLowerCase() == "yes" ? true : false
 		}
 
-		message.channel.send(`Are you sure to want to delete this file?`).then(() => {
+		message.util.send(`Are you sure to want to delete this file?`).then(() => {
 			message.channel.awaitMessages(filter, { max: 1, maxProcessed: 1, time: 15000, errors: ["time"] }).then(async collected => {
-				if (collected.size == 0) return message.channel.send("Deletion Aborted.");
+				if (collected.size == 0) return message.channel.send("Deletion Cancelled.");
 
 				await this.client.router.files.delete({ id: args.id });
-				message.channel.send(`File \`${args.id}\` has been deleted.`);
-			})
-		})
+				message.util.send(`File \`${args.id}\` has been deleted.`);
+			}).catch(() => {
+				message.util.send("Prompt has timed out, Cancelling delete.");
+			});
+		});
 	}
 }
